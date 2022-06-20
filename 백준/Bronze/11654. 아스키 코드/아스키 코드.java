f525fcba7241a2ddc7fbc21c23ec8dc80ab32ec0
Lee-Seungchan <103403612+Lee-Seungchan@users.noreply.github.com>
2022-06-20 22:57:20 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner sc = new Scanner(System.in);
+ 
+		String string = sc.next();
+        int as = string.charAt(0);
+        
+		System.out.print(as);
+	}
+}
