8e5acbde887eeba4db05ccbe0e053a6d45317aa2
Lee-Seungchan <103403612+Lee-Seungchan@users.noreply.github.com>
2022-07-05 09:06:46 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int A, B, C = 0;
+    
+    scanf("%d %d %d", &A, &B, &C);
+
+    if(B>=C) printf("-1\n");
+    else printf("%d\n", A/(C-B)+1);
+    
+    return 0;
+}
