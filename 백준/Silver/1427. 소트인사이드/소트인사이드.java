2d5e5ab39ef305146f37fea0131d571cbbbfbc6e
Lee-Seungchan <103403612+Lee-Seungchan@users.noreply.github.com>
2022-07-21 23:39:43 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int[] counting = new int[10];
+		String s = br.readLine();
+ 
+		for (int i = 0; i < s.length(); i++) {
+			counting[s.charAt(i) - '0'] ++;
+		}
+ 
+		for (int i = 9; i >= 0; i--) {
+			while (counting[i]-- > 0) {
+				System.out.print(i);
+			}
+		}
+	}
+}
